78058d1226ffb2c76bb96314975073bcbaad2198
sophia wang <sophia.xy.wang@gmail.com>
2024-12-21 16:51:06 -0800

    initial commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+/* Biodiversity Ecology - Mark and Recapture Model
+ * Written by Sophia Wang
+ * 12.21.2024
+*/
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	repaint();
+}
+
+function repaint() {
+	background(100);
+}
+
+function draw() {
+	repaint();
+}
